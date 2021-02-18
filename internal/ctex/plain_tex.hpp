d63efa39e35b63223a0c75c7faad9130721ce489
Sebastien Binet <s@sbinet.org>
2021-02-18 11:13:55 +0100

    internal/ctex: move tex class impl to .cpp
    Signed-off-by: Sebastien Binet <s@sbinet.org>

--- internal/ctex/plain_tex.hpp
+++ internal/ctex/plain_tex.hpp
@@ -7,18 +7,17 @@
 
 #include "tex.hpp"
 
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <string>
-
 
 namespace tex {
 
 class plain : public tex {
 
-  std::string output_path;  // directory to store output files in
-  std::string input_path;   // directory to search input files for
+  std::string output_path; // directory to store output files in
+  std::string input_path;  // directory to search input files for
 
   bool a_open_in(FILE *&ios) override;
   bool b_open_in(FILE *&ios) override;
@@ -28,17 +27,12 @@ class plain : public tex {
   void open_log_file() override;
 
 public:
-  virtual
-  void typeset(
-		  const std::string &filename,
-		  const std::string &result,
-		  const std::string &search_dir,
-		  const std::string &working_dir,
-		  const std::string &output);
+  virtual void typeset(const std::string &filename, const std::string &result,
+                       const std::string &search_dir,
+                       const std::string &working_dir,
+                       const std::string &output);
 };
-
 
 } // namespace tex
 
 #endif // CTEX_PLAIN_TEX_HPP
-
