3fe99135c6ca587934a69c316ac2896f928f2e21
Sebastien Binet <s@sbinet.org>
2021-02-18 11:23:12 +0100

    internal/ctex: add cgo I/O API
    Signed-off-by: Sebastien Binet <s@sbinet.org>

---
+++ internal/ctex/ctex-capi-io.h
@@ -0,0 +1,43 @@
+// Copyright ©2021 The star-tex Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#ifndef CTEX_CAPI_IO_H
+#define CTEX_CAPI_IO_H 1
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int ctex_io_file;
+
+extern
+ctex_io_file ctex_io_open(const char *name);
+
+extern
+ctex_io_file ctex_io_create(const char *name);
+
+extern
+int
+ctex_io_fflush(ctex_io_file fd);
+
+extern
+int
+ctex_io_fwrite(ctex_io_file fd, const uint8_t *data, const uint_t size);
+
+extern
+int
+ctex_io_fclose(ctex_io_file fd);
+
+extern
+int
+ctex_io_fwriteln(ctex_io_file fd);
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* CTEX_CAPI_IO_H */
