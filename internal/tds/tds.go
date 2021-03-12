6ae8ccc524bcad9553b9d638dd6f6ec74f2a436a
Sebastien Binet <s@sbinet.org>
2021-03-12 20:10:34 +0100

    internal/tds: first import
    Signed-off-by: Sebastien Binet <s@sbinet.org>

---
+++ internal/tds/tds.go
@@ -0,0 +1,13 @@
+// Copyright ©2021 The star-tex Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package tds provides a minimal TeX Directory Structure for star-tex.
+package tds // import "star-tex.org/x/tex/internal/tds"
+
+import "embed"
+
+//go:embed tex/plain/base/plain.tex
+//go:embed tex/generic/hyphen/hyphen.tex
+//go:embed fonts/tfm/public/cm/*.tfm
+var FS embed.FS
